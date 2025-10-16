b8eb3f38e8ff24942d94878be19cc35f8c9cf642
hzq-cat <hzqroslan59@gmail.com>
2025-10-16 11:31:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
-## import hzqcode as hzq
+
+## hzq code
+## ----------------------------------------------------------------------------------------------------------------------------------------
+
+coffee = {'Name' : 'Coffee',
+          'Image': 'https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?cs=srgb&dl=pexels-chevanon-312418.jpg&fm=jpg'
+          'Price' : 3,
+          'Type' : ['Mocha', 'Latte', 'Cappuccino']
+         }
+
+frjuice = {'Name' : 'Fruit Juice',
+          'Price' : 2,
+          'Type' : ['Apple', 'Lemon', 'Watermelon']
+          }
+
+cake = {'Name' : 'Cake',
+          'Price' : 6,
+          'Type' : ['Chocolate', 'Vanilla', 'Cheese']
+       }
+
+## ----------------------------------------------------------------------------------------------------------------------------------------
 
 st.set_page_config(page_title="CTD1D Café", page_icon="☕", layout="centered")
 
@@ -9,4 +29,4 @@ st.write("Each teammate can add a page inside the `pages/` folder to build new f
 st.write("MEOW")
 st.write("test")
 
-st.image('https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?cs=srgb&dl=pexels-chevanon-312418.jpg&fm=jpg')
+st.image(coffee['Image'])
