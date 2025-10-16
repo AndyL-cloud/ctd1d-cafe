c90edcd05331349ba5139a41299884923a1ea751
zwewaiyan0554-lgtm <zwewaiyan0554@gmail.com>
2025-10-16 11:33:36 +0800

    Update zwyc0d3.py

--- zwyc0d3.py
+++ zwyc0d3.py
@@ -1,3 +1,23 @@
+import streamlit as st
+#This function is to find the searched product in our dictionary
 def search_products(product):
+  numbers = []
+  special_characters = []
+  letters = []
+  #This is to ensure that the user input does not contain any excess spaces
   product = product.lower().strip()
-  for i in range(
+  for i in product:
+    if(i.isdigit()):
+      numbers.append(i)
+    elif(i.isalpha()):
+      letters.append(i)
+    else:
+      special_characters.append()
+  
+  
+      
+
+def order(category, sub_category, qty):
+  type_of_product = 
+  
+    
