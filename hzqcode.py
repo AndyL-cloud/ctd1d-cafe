1c9b55bbd3ea65a0e70df07a2647a0bb1702cc24
hzq-cat <hzqroslan59@gmail.com>
2025-10-16 10:32:02 +0800

    Update hzqcode.py

--- hzqcode.py
+++ hzqcode.py
@@ -1,1 +1,18 @@
-pass
+import streamlit as st
+
+coffee = {'Name' : 'Coffee',
+          'Price' : 3,
+          'Type' : ['Mocha', 'Latte', 'Cappuccino']
+         }
+
+frjuice = {'Name' : 'Fruit Juice',
+          'Price' : 2,
+          'Type' : ['Apple', 'Lemon', 'Watermelon']
+          }
+
+cake = {'Name' : 'Cake',
+          'Price' : 6,
+          'Type' : ['Chocolate', 'Vanilla', 'Cheese']
+       }
+
+print(coffee[2][1])
