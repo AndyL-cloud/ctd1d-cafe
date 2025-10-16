666116a094fbccb021555866bd1dfe69ef6cf4d0
hzq-cat <hzqroslan59@gmail.com>
2025-10-16 11:35:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,5 @@ st.set_page_config(page_title="CTD1D Café", page_icon="☕", layout="centered")
 st.title("☕ CTD1D Café — Team Streamlit App")
 st.write("Welcome to our collaborative Streamlit project!")
 st.write("Each teammate can add a page inside the `pages/` folder to build new features.")
-st.write("MEOW")
-st.write("test")
 
 st.image(coffee['Image'])
