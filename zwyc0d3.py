ee169e96392267f961ea67bc802610634eaac71e
zwewaiyan0554-lgtm <zwewaiyan0554@gmail.com>
2025-10-16 12:31:06 +0800

    Update zwyc0d3.py

--- zwyc0d3.py
+++ zwyc0d3.py
@@ -1,11 +1,15 @@
 import streamlit as st
+
 #This function is to find the searched product in our dictionary
 def search_products(product):
   numbers = []
   special_characters = []
   letters = []
+  
   #This is to ensure that the user input does not contain any excess spaces
   product = product.lower().strip()
+
+  #This loop is to sort the user input into separate lists accordingly. 
   for i in product:
     if(i.isdigit()):
       numbers.append(i)
@@ -13,6 +17,12 @@ def search_products(product):
       letters.append(i)
     else:
       special_characters.append()
+
+  combined_letters = ''.join(letters)
+
+  #This loop is to find the separated letter list in our dictionary of products. 
+  
+  
   
   
       
